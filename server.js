cb59ee66ac503525bb6799eb27020f530ac89e3b
Nayomekaia <nayomedoelwijt@gmail.com>
2025-11-02 23:19:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import { Liquid } from 'liquidjs'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // Liquid als view engine
 const engine = new Liquid({
   root: './views',      
   extname: '.liquid'    
 })
+// Liquid
+const engine = new Liquid({ root: './views', extname: '.liquid' })
 app.engine('liquid', engine.express())
 app.set('views', './views')
 app.set('view engine', 'liquid')
